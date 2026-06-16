decce1ea8fdda33e68c42a8d9b2736a8d1a82d6f
alessanzm <alessanzm@gmail.com>
2026-06-17 06:54:31 +0800

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,5 @@
-// This middleware can be expanded later to verify JWT tokens or session cookies
 exports.verifyAccess = (req, res, next) => {
     console.log(`[SECURITY LOG]: Incoming request to ${req.path}`);
     
-    // For now, it cleanly passes the request forward to the controllers
     next();
 };
