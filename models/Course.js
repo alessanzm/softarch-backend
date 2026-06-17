b4381f1d04630bd212092c1b5ccb7e9c56dc1e3b
alessanzm <alessanzm@gmail.com>
2026-06-17 13:55:04 +0800

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -1,33 +1,28 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-
     title: {
         type: String,
         required: true
     },
-
     courseCode: {
         type: String,
         required: true,
-        unique: true
+        unique: true 
     },
-
     description: {
         type: String,
         default: ''
     },
-
     lecturerId: {
         type: String,
         required: true
     },
-
-    students: {
-        type: [String],
+    students: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         default: []
-    }
-
+    }]
 }, {
     timestamps: true
 });
