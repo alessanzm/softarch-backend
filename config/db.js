1ab87c21491f66cdff8fd386d9aeec254a261c92
alessanzm <alessanzm@gmail.com>
2026-06-20 11:23:23 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -10,15 +10,15 @@ const connectDB = async () => {
     const uri = process.env.MONGO_URI;
     
     if (!uri) {
-      console.error('❌ Ralat Kritikal: process.env.MONGO_URI tidak dikesan!');
+      console.error(' Ralat Kritikal: process.env.MONGO_URI tidak dikesan!');
       console.error('Sila pastikan nama KEY di dashboard Render dieja MONGO_URI dengan huruf besar.');
       throw new Error("MONGO_URI is undefined");
     }
 
     await mongoose.connect(uri);
-    console.log('✨ Berjaya disambungkan ke MongoDB Atlas via Mongoose!');
+    console.log(' Berjaya disambungkan ke MongoDB Atlas via Mongoose!');
   } catch (error) {
-    console.error('❌ Gagal menyambung ke MongoDB:', error.message);
+    console.error('Gagal menyambung ke MongoDB:', error.message);
     process.exit(1); 
   }
 };
